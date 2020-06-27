e1ec651ad990bc97e99df71894ec5f449481f49a
fakhri ahnaf <fakhri_ahnaf01@yahoo.co.id>
2020-06-27 16:02:10 +0700

    Initial Commit

---
+++ CuitanKu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuitanKu
+//
+//  Created by Fakhri Ahnaf Dhia on 27/06/20.
+//  Copyright © 2020 Fakhri Ahnaf Dhia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
